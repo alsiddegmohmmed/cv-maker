68041e94a7239d47a8d5e8b18af42c18f7d719ef
eldarlrd <eldarlrd@pm.me>
2024-03-21 18:06:13 +0400

    feat(banners): header & footer

--- src/components/banners/Footer.tsx
+++ src/components/banners/Footer.tsx
@@ -1,3 +1,18 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { type ReactElement } from 'react';
 
-export const Footer = (): ReactElement => <></>;
+export const Footer = (): ReactElement => (
+  <footer>
+    © 2024{' '}
+    <a
+      title='Source'
+      target='_blank'
+      type='text/html'
+      className='external'
+      href='https://github.com/eldarlrd/cv-maker'
+      rel='noopener noreferrer nofollow external author'>
+      <FontAwesomeIcon icon={faGithub} /> eldarlrd
+    </a>
+  </footer>
+);
